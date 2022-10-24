f34af8ad85ad4a5c5d9167f52c76fed4fe8106b9
Kyle Scott <kyscott@umich.edu>
2022-10-24 16:54:10 -0400

    update callback validation

--- src/libraries/CallbackValidation.sol
+++ src/libraries/CallbackValidation.sol
@@ -1,29 +1,22 @@
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
 
 import { Lendgine } from "numoen-core/Lendgine.sol";
 import { Factory } from "numoen-core/Factory.sol";
-
-import { LendgineAddress } from "./LendgineAddress.sol";
+import { LendgineAddress } from "numoen-core/libraries/LendgineAddress.sol";
 
 library CallbackValidation {
     error VerifyError();
 
     function verifyCallback(address factory, LendgineAddress.LendgineKey memory lendgineKey) internal view {
-        address lendgine = Factory(factory).getLendgine(
+        address lendgine = LendgineAddress.computeAddress(
+            factory,
             lendgineKey.base,
             lendgineKey.speculative,
+            lendgineKey.baseScaleFactor,
+            lendgineKey.speculativeScaleFactor,
             lendgineKey.upperBound
         );
         if (msg.sender != lendgine) revert VerifyError();
-    }
-
-    function verifyPairCallback(address factory, LendgineAddress.LendgineKey memory lendgineKey) internal view {
-        address lendgine = Factory(factory).getLendgine(
-            lendgineKey.base,
-            lendgineKey.speculative,
-            lendgineKey.upperBound
-        );
-        address pair = Lendgine(lendgine).pair();
-        if (msg.sender != pair) revert VerifyError();
     }
 }
